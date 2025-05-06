3c5e1c37ed457b34688f861ba6a047406256c0e7
sarahrittner <sarah.rittner@petersgasse.at>
2025-05-06 21:36:53 +0200

    first commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public InputAction LeftAction;
+    public InputAction MoveAction;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        MoveAction.Enable();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        Vector2 move = MoveAction.ReadValue<Vector2>();
+        Debug.Log("Move: " + move);
+        Vector2 position = (Vector2)transform.position + move * 0.01f;
+        transform.position = position;
+
+
+    
+    }
+}
