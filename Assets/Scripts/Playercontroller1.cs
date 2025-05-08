3d79c94601d1daaab5846359b8362de395a1fae0
sarahrittner <sarah.rittner@petersgasse.at>
2025-05-08 21:24:46 +0200

    add health stat, speed regulater in unity, tile collider

--- Assets/Scripts/Playercontroller1.cs
+++ Assets/Scripts/Playercontroller1.cs
@@ -4,41 +4,54 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 
 
-public class Playercontroller : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
   // Variables related to player character movement
   public InputAction MoveAction;
   public InputAction MoveActionwasd;
   Rigidbody2D rigidbody2d;
   Vector2 move;
+  public float speed = 3.0f;
+
+
+  // Variables related to the health system
+  public int maxHealth = 5;
+  int currentHealth;
 
 
   // Start is called before the first frame update
   void Start()
   {
      MoveAction.Enable();
-
-     MoveActionwasd.Enable();
-    
      rigidbody2d = GetComponent<Rigidbody2D>();
+     currentHealth = maxHealth;
   }
  
   // Update is called once per frame
   void Update()
   {
-     move = MoveActionwasd.ReadValue<Vector2>();     
      move = MoveAction.ReadValue<Vector2>();
-
+        if (move == Vector2.zero)
+        {
+            move = MoveActionwasd.ReadValue<Vector2>();
+        }
      Debug.Log(move);
-
   }
 
 
-  void FixedUpdate()
+  // FixedUpdate has the same call rate as the physics system 
+ void FixedUpdate()
   {
-     Vector2 position = (Vector2)rigidbody2d.position + move * 3.0f * Time.deltaTime;
+     Vector2 position = (Vector2)rigidbody2d.position + move * speed * Time.deltaTime;
      rigidbody2d.MovePosition(position);
-
-
   }
+
+
+  void ChangeHealth (int amount)
+  {
+     currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+     Debug.Log(currentHealth + "/" + maxHealth);
+  }
+
+
 }
