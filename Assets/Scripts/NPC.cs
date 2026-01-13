c90550a4c6b76a671f8a98b664c642ee6fa70b6b
sarahrittner <sarah.rittner@petersgasse.at>
2026-01-13 22:43:40 +0100

    Implement NPC class with pathfinding and patrol behavior

---
+++ Assets/Scripts/NPC.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class NPC : MonoBehaviour
+{
+    public int maxHealth = 100;
+    public int curHealth;
+    public int panicMultiplier = 1;
+
+
+    public Node currentNode;
+    public List<Node> path = new List<Node>();
+
+
+
+
+
+    public PlayerController player;
+
+    public float speed = 3f;
+
+    private void Start()
+    {
+        curHealth = maxHealth;
+    }
+
+    private void Update()
+    {
+        CreatePath();
+
+        Patrol();
+    }
+
+    void Patrol()
+    {
+        if(path.Count == 0)
+        {
+            path = AStarManager.instance.GeneratePath(currentNode, AStarManager.instance.AllNodes()[Random.Range(0, AStarManager.instance.AllNodes().Length)]);
+        }
+    }
+
+    public void CreatePath()
+    {
+        if (path.Count > 0)
+        {
+            int x = 0;
+            transform.position = Vector3.MoveTowards(transform.position, new Vector3(path[x].transform.position.x, path[x].transform.position.y, -2), (speed * panicMultiplier) * Time.deltaTime);
+
+            if (Vector2.Distance(transform.position, path[x].transform.position) < 0.1f)
+            {
+                currentNode = path[x];
+                path.RemoveAt(x);
+            }
+        }
+    }
+}
